94fc701e762d016269741961ae948ef31c34de71
jia.zhang@aiseeding.com <jia.zhang@aiseeding.com>
2021-01-22 13:39:14 +0800

    计划学习的大方向

---
+++ 00_小海腾的学习计划.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/22 13:06
+# @Author  : 小海腾
+
+
+# 1、机器学习算法
+# 2、前端知识
+# 3、crm客户关系管理系统
+# 4、搜索引擎
+# 5、docker使用以及Kubernetes和Swarm
+# 6、go/java
+# 7、编程范式与设计模式
