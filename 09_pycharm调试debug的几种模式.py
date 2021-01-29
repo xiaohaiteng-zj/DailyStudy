bebba0a00db813ea391651e4bcbd87e8fa0106e2
jia.zhang@aiseeding.com <jia.zhang@aiseeding.com>
2021-01-29 16:41:04 +0800

    pycharm调试debug的几种模式

---
+++ 09_pycharm调试debug的几种模式.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/29 16:31
+# @Author  : 小海腾
+# 学习链接   : https://blog.csdn.net/zhangvalue/article/details/97640752
+
+
+# 一、debug的step by step
+# step over —— 遇到函数不会跳进去，而是平躺过去
+# step into —— 遇到函数跳进去
+# step out —— 从函数里面跳出来。即跳到上一级。
+# step into my code —— 忽略库代码，只在业务代码跳
+# run to cursor —— 代码执行到光标所在的那行
+
+
+# 二、 查看表达式结果
+# Evaluate Expression：
+# ①、可以即时求值
+# ②、不但可以运行表达式，还可以切换到代码片段模式，运行代码片段
+
+
+# 三、单个变量查看
+# 1、 鼠标悬停在变量上
+# 2、 打开watch窗口
+
