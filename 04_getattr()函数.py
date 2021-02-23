27e7ce924287f6e45fa925769637f71a84016a9f
jia.zhang@aiseeding.com <jia.zhang@aiseeding.com>
2021-02-23 16:40:00 +0800

    2.23 学习go,第一天

--- 04_getattr()函数.py
+++ 04_getattr()函数.py
@@ -9,9 +9,26 @@ import numpy as np
 # getattr(object, name[, default])
 df = pd.DataFrame({'a': [1, 2, 3], 'b': ['a', 'b', 'c']})
 print(df)
+tmp_list = []
 for row in df.itertuples():
     # row是一个对象Pandas(Index=0, a=1, b='a')
     print(row)
+    tmp_list.append({getattr(row, 'b'): getattr(row, 'a')})
     re = getattr(row, 'b')
     print(re)
+print(tmp_list)
 
+dic = {"wh": "bj"}
+
+li = [
+    {"bj": "wh"},
+    {"bj": "cd"},
+    {"tc": "wh"},
+    {"tc": "gz"},
+]
+
+if {"bj": "wh"} in li:
+    print("hahahahh")
+
+if {dic["wh"]: 'wh'} in li:
+    print("11111111111111111111")
